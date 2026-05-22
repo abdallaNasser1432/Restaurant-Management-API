13a1cb96ac4f79b9d885289a4a4b95007caf3b1f
Abdalla Nasser <abdallanaser1432000@gmail.com>
2026-05-22 08:45:28 +0300

    Add global error handling and response helpers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const notFound = require('./middlewares/notFound.middleware');
+const errorHandler = require('./middlewares/error.middleware');
+
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Restaurant Management API is running',
+  });
+});
+
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
